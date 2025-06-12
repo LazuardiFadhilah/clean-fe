11afafec3bc6c437fffa5895d99493d2a52bf058
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2025-06-12 21:26:25 +0700

    feat: add article cards and footer section to DesktopHeroSection component

--- app/desktopSection.tsx
+++ app/desktopSection.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/components/ui/select";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import ArticleCard from "@/components/articleCard";
 
 export default function DesktopHeroSection() {
   const [bedroom, setBedroom] = useState("");
@@ -149,13 +150,122 @@ export default function DesktopHeroSection() {
           Why Choose <br /> Shield ?
         </h1>
         <p className="text-left font-light text-xl text-neutral-100 w-full lg:max-w-2xl">
-          We understand your home is important to you. That&apos;s why we focus on
-          the quality of the clean. Our cleaners aren&apos;t contract workers - they
-          are full-time employees. They care as much as we do.
+          We understand your home is important to you. That&apos;s why we focus
+          on the quality of the clean. Our cleaners aren&apos;t contract workers
+          - they are full-time employees. They care as much as we do.
         </p>
       </div>
       <div className="flex w-full px-6 lg:px-30 mt-40 items-center justify-center">
         <img src="./content4.png" alt="" />
+      </div>
+      <div className="flex flex-col items-center justify-center w-full mt-30 py-25 bg-neutral-100 bg-[url('/bg1.png')] bg-cover">
+        <h1 className="text-4xl xl:text-5xl text-white font-semibold">
+          The Shield Report
+        </h1>
+
+        <div className="flex flex-col xl:flex-row items-stretch justify-center mt-10 w-full px-6 lg:px-30 gap-6">
+          {/* CARD 1 */}
+          <div className="flex-1 basis-1/3 flex">
+            <div className="w-full h-full flex flex-col">
+              <ArticleCard
+                image="/img1.png"
+                title="How to Efficiently Clean & Organize Living Areas :"
+                description="November is here, and with it comes a fresh opportunity to tackle the clutter and dust that may have account for with a little sploo..."
+                authorImage="./pp1.jpg"
+                authorName="Budiono"
+                date="June 9, 2025"
+              />
+            </div>
+          </div>
+
+          {/* CARD 2 */}
+          <div className="flex-1 basis-1/3 flex">
+            <div className="w-full h-full flex flex-col">
+              <ArticleCard
+                image="/img2.png"
+                title="How to Create a Self-Cleaning Home From Scratch."
+                description="Creating a home that practically cleans itself may sound like a dream, but with a little know-how and some for with a little sploo..."
+                authorImage="./pp2.jpg"
+                authorName="Freya"
+                date="June 9, 2025"
+              />
+            </div>
+          </div>
+
+          {/* CARD 3 */}
+          <div className="flex-1 basis-1/3 flex">
+            <div className="w-full h-full flex flex-col">
+              <ArticleCard
+                image="/img3.png"
+                title="10 Easy Ways to Turn Homekeeping, Happy!"
+                description="Homekeeping can sometimes feel like a never-ending to-do list that zaps the joy right out of your day. But when the tides turn be..."
+                authorImage="./pp3.jpg"
+                authorName="Ella"
+                date="June 9, 2025"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-start justify-between w-full mt-30 p-25">
+        <div className="flex flex-row justify-between w-full ">
+          <div className="flex flex-row gap-[30px]">
+            <img src="./medsos1.png" className="w-[8px] h-[18px]" alt="" />
+            <img src="./medsos2.png" className="w-[18px] h-[15px]" alt="" />
+            <img src="./medsos3.png" className="w-[17px] h-[17px]" alt="" />
+            <img src="./medsos4.png" className="w-[18px] h-[17px]" alt="" />
+          </div>
+          <div className="flex flex-row gap-[30px] lg:gap-[100px]">
+            <div className="flex flex-col gap-[10px]">
+            <h1 className="mb-[15px] text-sm text-start font-bold text-neutral-100">
+              COMPANY
+            </h1>
+            <p className="text-sm text-start font-light text-neutral-400">
+              About Us
+            </p>
+            <p className="text-sm text-start font-light text-neutral-400">
+              Career
+            </p>
+            <p className="text-sm text-start font-light text-neutral-400">
+              Press
+            </p>
+            <p className="text-sm text-start font-light text-neutral-400">
+              Blog
+            </p>
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <h1 className="mb-[15px] text-sm text-start font-bold text-neutral-100">
+              SERVICES
+            </h1>
+            <p className="text-sm text-start font-light text-neutral-400">
+              Residential
+            </p>
+            <p className="text-sm text-start font-light text-neutral-400">
+              Office Cleaning
+            </p>
+            <p className="text-sm text-start font-light text-neutral-400">
+              Commercial Cleaning
+            </p>
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <h1 className="mb-[15px] text-sm text-start font-bold text-neutral-100">
+              SUPPORT
+            </h1>
+            <p className="text-sm text-start font-light text-neutral-400">
+              Contact Us
+            </p>
+            <p className="text-sm text-start font-light text-neutral-400">
+              FA&Q&apos;s
+            </p>
+          </div>
+          </div>
+        </div>
+        <div className="flex flex-row items-center justify-center mt-10 gap-[20px]">
+          <img src="./Logo2.png" className="w-fit h-[40px]" alt="" />
+          <p className="text-sm text-neutral-200 font-light text-center mt-4">
+            © Clean Co. All Rights Reserved . Terms of Service . Privacy Policy
+          </p>
+        </div>
       </div>
     </>
   );
