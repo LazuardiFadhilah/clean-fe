a36f2dd4aa1d937bd0fae1fb01dd7311691d3f9a
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2025-06-25 23:43:21 +0700

    fix: adjust layout for login page by centering buttons and container

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Login() {
   return (
     <>
-      <div className="flex flex-col items-center container mt-10">
+      <div className="flex flex-col items-center container mx-auto mt-10">
         <div className="flex justify-center">
          <Link href="/">
           <img
@@ -15,13 +15,15 @@ export default function Login() {
         <h1 className="text-center text-2xl md:text-4xl font-bold text-neutral-100 mt-[72px]">
           Login
         </h1>
-        <Button variant="outline" className="max-w-[calc(100%-50px)] w-full mt-[35px] md:w-[382px] border-grey-800 border-2 text-neutral-100 font-semibold text-sm">
+        <div className="flex flex-col items-center w-full">
+          <Button variant="outline" className="max-w-[calc(100%-50px)] w-full mt-[35px] md:w-[382px] border-grey-800 border-2 text-neutral-100 font-semibold text-sm">
          <img src="./google.png" className="w-[18px] h-[18px]" alt="" /> Continue with Google
         </Button>
         <Button variant="outline" className="max-w-[calc(100%-50px)] w-full mt-[20px] md:w-[382px] border-grey-800 border-2 text-neutral-100 font-semibold text-sm">
          <img src="./apple.png" className="w-[18px] h-[18px]" alt="" /> Sign up with Apple
         </Button>
         <div className="w-full max-w-[calc(100%-50px)] md:w-[382px] h-[2px] bg-grey-900 mt-[20px]"/>
+        </div>
       </div>
     </>
   );
